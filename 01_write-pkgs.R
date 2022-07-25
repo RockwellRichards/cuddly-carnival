a60ddddf0e71371ec8b1be1dc7c2e83e3a831286
Rockwell <rrichards@uri.edu>
2022-07-25 16:49:56 -0400

    sitch to github doc

--- 01_write-pkgs.R
+++ 01_write-pkgs.R
@@ -1,4 +1,10 @@
 #' Installed packages
+#' #' ---
+#' #' author: "RR"
+#' date: "`r format(Sys.Date())`
+#' output: github_document
+#' ---
+
 library(tidyverse)
 ipt <- installed.packages() %>%
   as_tibble()
